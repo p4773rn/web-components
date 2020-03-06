043cf0fc0a3cd244649b09f1437862529aaf1bb3
p4773rn <alisher.shakhiyev@nu.edu.kz>
2020-03-06 19:36:04 +0600

    Add typography

--- AppCard.js
+++ AppCard.js
@@ -1,28 +1,60 @@
-const appCard = {}
-appCard.innerHTML = `
-<div class="app-card">
+const appCardTemplate = document.createElement('template')
+appCardTemplate.innerHTML = `
+
+<style>
+/* Style */
+:host {
+  display: block;
+  margin: var(--all-1);
+  width: 500px;
+  box-shadow: var(--elevation-2dp);
+  background: var(--surface);
+}
+.header {
+  background: var(--surface-header);
+  padding: var(--all-2);
+}
+app-typography {
+  text-align: left;
+  color: var(--on-surface);
+}
+.content {
+  padding: var(--all-2);
+}
+</style>
+
+<!-- Template -->
+<div class="header">
+  <app-typography type="subtitle-1"></app-typography>
+</div>
+<div class="content">
   <slot></slot>
 </div>
-<style>
-    .app-card {
-        padding: var(--all-2);
-        margin: var(--all-1);
-        width: 500px;
-        box-shadow: var(--elevation-4dp);
-        background: var(--surface);
-    }
-</style>
 `
 
 class AppCard extends HTMLElement {
-  constructor() {
-    super()
-    const shadowRoot = this.attachShadow({ mode: 'open' })
-    shadowRoot.innerHTML = appCard.innerHTML
+  static get observedAttributes() {
+    return ['headline']
   }
 
-  static get observedAttributes() {
-    return ['flat']
+  constructor() {
+    super()
+    this._shadowRoot = this.attachShadow({ mode: 'open' })
+    this.shadowRoot.innerHTML = appCardTemplate.innerHTML
+    this.$headline = this._shadowRoot.querySelector('app-typography')
+  }
+
+  connectedCallback() {
+    this.$headline.setAttribute('text', this.getAttribute('headline'))
+  }
+
+  // Getters and setters
+  get headline() {
+    return this.getAttribute('headline')
+  }
+
+  set headline(val) {
+    this.setAttribute('headline', val)
   }
 }
 
